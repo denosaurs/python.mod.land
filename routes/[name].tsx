34d0d47357f912ee7d567d6b8c014724c29925ef
loading <49134864+load1n9@users.noreply.github.com>
2022-03-29 11:02:22 -0400

    feat: speed boost

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -3,11 +3,10 @@ import { Handlers } from "../server_deps.ts";
 
 export const handler: Handlers = {
   GET(_, ctx) {
-    const id = crypto.randomUUID().replace("-","");
     const mod =
 `import { python } from "https://deno.land/x/python@0.1.4/mod.ts";
-export const ${id} = python.import("${ctx.params.name}");
-export default ${id};`;
+export const python_mod = python.import("${ctx.params.name}");
+export default python_mod;`;
     return new Response(mod, {
       headers: { "Content-Type": "application/javascript" },
     });
