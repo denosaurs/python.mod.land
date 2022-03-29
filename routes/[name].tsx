114a49f0fb1c0b1e93778455215d45da8a0d3f83
loading <49134864+load1n9@users.noreply.github.com>
2022-03-29 10:57:45 -0400

    Update [name].tsx

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -3,10 +3,11 @@ import { Handlers } from "../server_deps.ts";
 
 export const handler: Handlers = {
   GET(_, ctx) {
+    const id = crypto.randomUUID();
     const mod =
 `import { python } from "https://deno.land/x/python@0.1.4/mod.ts";
-export const ${ctx.params.name} = python.import("${ctx.params.name}");
-export default ${ctx.params.name};`;
+export const ${id} = python.import("${ctx.params.name}");
+export default ${id};`;
     return new Response(mod, {
       headers: { "Content-Type": "application/javascript" },
     });
