1592915f73f9a8423ec826aebc15e0cc5f3d689a
loading <49134864+load1n9@users.noreply.github.com>
2022-03-29 10:29:12 -0400

    Update List.tsx

--- components/List.tsx
+++ components/List.tsx
@@ -6,7 +6,7 @@ import { h, tw, useState, useEffect } from "../client_deps.ts";
 export function List() {
     const [data, setData] = useState(null);
     useEffect(() => {
-        fetch("https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json").then(r => r.json()).then(r => setData(r)).then(r => console.log(r))
+        fetch("https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json").then(r => r.json()).then(r => setData(r));
     }, []);
     const container = tw`max-w-screen-sm mx-auto px(4 sm:4 md:4) space-y-3`;
     const list = tw`max-w-screen-sm mt-4 text(xl gray-600 center)`;
@@ -14,9 +14,7 @@ export function List() {
     return (
         <div class={container}>
             <ul class={list}>
-                Top Modules
             </ul>
         </div>
     );
 }
-// https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json
