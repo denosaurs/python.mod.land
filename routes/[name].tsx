6f4bf964a227881600572d0f134524473bdf460b
loading <49134864+load1n9@users.noreply.github.com>
2022-03-29 15:36:03 -0400

    feat: check if module exists

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,12 +1,17 @@
 /** @jsx h */
 import { Handlers } from "../server_deps.ts";
-
+import { checkExist } from "../utils.ts";
 export const handler: Handlers = {
   GET(_, ctx) {
-    const mod =
-`import { python } from "https://deno.land/x/python@0.1.4/mod.ts";
+    let exists: boolean;
+    checkExist(ctx.params.name, (_exists: boolean) => {
+      exists = _exists;
+    });
+    const mod = exists!
+      ? `import { python } from "https://deno.land/x/python@0.1.4/mod.ts";
 export const python_mod = python.import("${ctx.params.name}");
-export default python_mod;`;
+export default python_mod;`
+      : `404 not found`;
     return new Response(mod, {
       headers: { "Content-Type": "application/javascript" },
     });
